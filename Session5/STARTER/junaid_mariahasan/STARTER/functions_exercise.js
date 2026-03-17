31b7af52e45a999ef694f77bca12e00dd0bf306a
Junaid Maria Hasan <junaid.mariahasan@uzh.ch>
2026-03-17 13:24:16 +0100

    Sync_1

--- Session5/STARTER/junaid_mariahasan/STARTER/functions_exercise.js
+++ Session5/STARTER/junaid_mariahasan/STARTER/functions_exercise.js
@@ -6,8 +6,8 @@
 const threshold = 300; 
 function isFasterThanThreshold(responseTime) {
     // Write logic to check if the reaction time is faster than the threshold
-    if(responseTime < threshold) {console.log("too fast");}
-    else {console.log("valid");}
+    const result = responseTime < threshold? "too fast":"valid"; 
+    return result;
     }
   
 // Call the function with different inputs
@@ -22,7 +22,15 @@ console.log(`Expected output: ${isFasterThanThreshold(400)}`)
 // call the function e.g., calculateAverageRT(responseTimes); .
 
 // Write your function here
-
+function calculateAverageRT(responseTimes){
+  let totalTime = 0;
+  let i = 0
+  for(time of responseTimes){
+    totalTime += time;
+    i ++; 
+  }
+  return totalTime/i;
+}
 
 
   // Example input
@@ -40,4 +48,19 @@ console.log(`Expected output: ${isFasterThanThreshold(400)}`)
     2. The console.log statement prints out the feedback for each response time in the array
 
 */
+const getPerformanceFeedback = (responseTime) =>{
+  let msg;
+  if (responseTime < 200 && responseTime > 0){
+    msg = "Excellent";
+  }else if(responseTime >= 200 && responseTime <= 500){
+    msg = "Good";
+  }
+  else{
+    msg = "Needs Improvement";
+  }
+  return msg;
+}
+for(let responseTime of responseTimes){
+  console.log(`${responseTime} ms, `, getPerformanceFeedback(responseTime));
+}
 
--- Session5/STARTER/junaid_mariahasan/STARTER/loops_exercise.js
+++ Session5/STARTER/junaid_mariahasan/STARTER/loops_exercise.js
@@ -10,10 +10,12 @@
  // console.log each iterate index and each reactionTime value accordingly in the loop
  let reactionTimes = [];
  // add your code here
-for (let i = 0; i < recordedTimes.length; i++) {
-    let reactionTime = recordedTimes[i] - 200;
-    reactionTimes.push(reactionTime);
-} 
+for(i = 0; i < 10; i++){
+    reactionTimes[i] = recordedTimes [i] -200;
+    console.log("iterate:", i);
+    console.log("recordedTime:", reactionTimes[i]);
+ }
+ console.log("reactionTimes: ",reactionTimes);
  
  /* # Plus 1: can you think of more than one way of doing it? */
  
@@ -32,12 +34,10 @@ let totalTime = 0;
 let thisTrial;
 let j = 0
 
-while (totalTime <= 2000 && j < reactionTimes.length) {
-    totalTime += reactionTimes[j];
-    
-    thisTrial = j + 1; // because trials start at 1
-    
-    j++;
+while (totalTime < 2000) {
+    totalTime = totalTime + reactionTimes [j];
+    thisTrial = j + 1;
+    j ++;
 }
 
  console.log(`Cumulative reaction time exceeded 2000 ms at trial ${thisTrial}.`);
